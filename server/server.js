308e8dc60a1bb5380744b8faa8c765d379c5a29f
sapey <alexandre.froment92@gmail.com>
2023-01-12 11:31:46 +0100

    clean

--- server/server.js
+++ server/server.js
@@ -45,4 +45,4 @@ app.post('/', async (req, res) => {
   }
 })
 
-app.listen(1000, () => console.log('AI server started on http://localhost:1000'))
+app.listen(5001, () => console.log('AI server started on http://localhost:5001'))
